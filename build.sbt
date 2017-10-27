4df641fe1ddb73d991293b89135cf6652f12b40c
Teddy Widom <theodore.widom@gmail.com>
2017-10-27 15:46:08 -0400

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "DBSubsetter"
+
+version := "0.1"
+
+scalaVersion := "2.12.3"
---
+++ src/main/scala/trw/dbsubsetter/DbSubsetter.scala
@@ -0,0 +1,6 @@
+package trw.dbsubsetter
+
+object DbSubsetter extends App {
+  //  val connectionString = "jdbc:postgresql://localhost:5432/db_subsetter"
+  //  val dbConnection = DriverManager.getConnection(connectionString)
+}
