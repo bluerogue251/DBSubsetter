34afd872d749892f3f77fc01cddbe28217e7f576
Teddy Widom <theodore.widom@gmail.com>
2017-11-14 23:42:18 -0500

    More perf optimizations, this time not blind

--- src/main/scala/trw/dbsubsetter/workflow/RowsToTasksConverter.scala
+++ src/main/scala/trw/dbsubsetter/workflow/RowsToTasksConverter.scala
@@ -4,25 +4,22 @@ import trw.dbsubsetter.db.{SchemaInfo, _}
 
 object RowsToTasksConverter {
   def convert(table: Table, rows: Vector[Row], sch: SchemaInfo, fetchChildren: Boolean): Vector[FkTask] = {
-    val parentTasks = for {
-      row <- rows
-      fk <- sch.fksFromTable(table)
-      cols = fk.fromCols
-      values = cols.map(c => row(c.name)) if !values.contains(null)
-    } yield FkTask(fk.toTable, fk, values, fetchChildren = false)
+    // `distinct` is a performance improvement which prevents duplicate tasks from being created
+    // As far as I can tell, it is only necessary for parent tasks
+    val parentTasks = sch.fksFromTable(table).toVector.flatMap { fk =>
+      val distinctFkValues = rows.map(row => fk.fromCols.map(_.ordinalPosition).map(row)).distinct
+      distinctFkValues.map(fkValue => FkTask(fk.toTable, fk, fkValue, fetchChildren = false))
+    }
 
-    val childTasks = if (!fetchChildren) {
-      Seq.empty
-    } else for {
-      row <- rows
-      fk <- sch.fksToTable(table)
-      cols = fk.toCols
-      values = cols.map(c => row(c.name)) if !values.contains(null)
-    } yield FkTask(fk.fromTable, fk, values, fetchChildren = true)
+    val childTasks = if (fetchChildren) {
+      sch.fksToTable(table).toVector.flatMap { fk =>
+        val fkValues = rows.map(row => fk.toCols.map(_.ordinalPosition).map(row))
+        fkValues.map(fkValue => FkTask(fk.fromTable, fk, fkValue, fetchChildren = true))
+      }
+    } else {
+      Vector.empty
+    }
 
-    // `distinct` is a performance improvement.
-    // It prevents us from later on needing to check the pkStore for the same values over and over again
-    // Is there a better way to achieve this same performance gain, e.g. by using a HashMap or a Set instead of a Seq?
-    parentTasks.distinct ++ childTasks
+    parentTasks ++ childTasks
   }
 }
