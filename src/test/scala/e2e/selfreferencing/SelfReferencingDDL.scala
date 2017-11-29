6ea712c60f7ccfa7996b4f9927ad3d2090849015
Teddy Widom <theodore.widom@gmail.com>
2017-11-29 15:40:11 -0500

    Fix self referencing test

--- src/test/scala/e2e/selfreferencing/SelfReferencingDDL.scala
+++ src/test/scala/e2e/selfreferencing/SelfReferencingDDL.scala
@@ -11,14 +11,14 @@ trait SelfReferencingDDL {
   /**
     * self_referencing_table
     */
-  case class SelfReferencingTableRow(id: Int, parentId: Int)
+  case class SelfReferencingTableRow(id: Int, parentId: Option[Int])
 
   class SelfReferencingTable(_tableTag: Tag) extends profile.api.Table[SelfReferencingTableRow](_tableTag, "self_referencing_table") {
     def * = (id, parentId) <> (SelfReferencingTableRow.tupled, SelfReferencingTableRow.unapply)
 
     val id: Rep[Int] = column[Int]("id", O.AutoInc, O.PrimaryKey)
-    val parentId: Rep[Int] = column[Int]("parent_id")
-    lazy val selfReferencingTableFk = foreignKey("self_referencing_table_parent_id_fkey", parentId, SelfReferencingTable)(_.id, onUpdate = ForeignKeyAction.NoAction, onDelete = ForeignKeyAction.NoAction)
+    val parentId: Rep[Option[Int]] = column[Option[Int]]("parent_id")
+    lazy val selfReferencingTableFk = foreignKey("self_referencing_table_parent_id_fkey", parentId, SelfReferencingTable)(r => Rep.Some(r.id), onUpdate = ForeignKeyAction.NoAction, onDelete = ForeignKeyAction.NoAction)
   }
 
   lazy val SelfReferencingTable = new TableQuery(tag => new SelfReferencingTable(tag))
--- src/test/scala/e2e/selfreferencing/SelfReferencingDML.scala
+++ src/test/scala/e2e/selfreferencing/SelfReferencingDML.scala
@@ -10,23 +10,39 @@ class SelfReferencingDML(val profile: JdbcProfile) extends SelfReferencingDDL {
     slick.dbio.DBIO.seq(
       SelfReferencingTable ++= Seq(
         // edge case: it references not only its own table, but its own row in that table
-        SelfReferencingTableRow(1, 1),
-        SelfReferencingTableRow(2, 1),
-        SelfReferencingTableRow(3, 2),
-        SelfReferencingTableRow(4, 7),
-        SelfReferencingTableRow(5, 1),
-        SelfReferencingTableRow(6, 3),
-        SelfReferencingTableRow(7, 4),
-        SelfReferencingTableRow(8, 1),
-        SelfReferencingTableRow(9, 1),
-        SelfReferencingTableRow(10, 2),
-        SelfReferencingTableRow(11, 3),
-        SelfReferencingTableRow(12, 4),
-        SelfReferencingTableRow(13, 5),
-        SelfReferencingTableRow(14, 6),
-        SelfReferencingTableRow(15, 7),
-        SelfReferencingTableRow(16, 15)
-      )
+        SelfReferencingTableRow(1, None), // 1),
+        SelfReferencingTableRow(2, None), // 1),
+        SelfReferencingTableRow(3, None), // 2),
+        SelfReferencingTableRow(4, None), // 7),
+        SelfReferencingTableRow(5, None), // 1),
+        SelfReferencingTableRow(6, None), // 3),
+        SelfReferencingTableRow(7, None), // 4),
+        SelfReferencingTableRow(8, None), // 1),
+        SelfReferencingTableRow(9, None), // 1),
+        SelfReferencingTableRow(10, None), //  2),
+        SelfReferencingTableRow(11, None), //  3),
+        SelfReferencingTableRow(12, None), //  4),
+        SelfReferencingTableRow(13, None), //  5),
+        SelfReferencingTableRow(14, None), //  6),
+        SelfReferencingTableRow(15, None), //  7),
+        SelfReferencingTableRow(16, None) //  15)
+      ),
+      SelfReferencingTable.filter(_.id === 1).map(_.parentId).update(Some(1)),
+      SelfReferencingTable.filter(_.id === 2).map(_.parentId).update(Some(1)),
+      SelfReferencingTable.filter(_.id === 3).map(_.parentId).update(Some(2)),
+      SelfReferencingTable.filter(_.id === 4).map(_.parentId).update(Some(7)),
+      SelfReferencingTable.filter(_.id === 5).map(_.parentId).update(Some(1)),
+      SelfReferencingTable.filter(_.id === 6).map(_.parentId).update(Some(3)),
+      SelfReferencingTable.filter(_.id === 7).map(_.parentId).update(Some(4)),
+      SelfReferencingTable.filter(_.id === 8).map(_.parentId).update(Some(1)),
+      SelfReferencingTable.filter(_.id === 9).map(_.parentId).update(Some(1)),
+      SelfReferencingTable.filter(_.id === 10).map(_.parentId).update(Some(2)),
+      SelfReferencingTable.filter(_.id === 11).map(_.parentId).update(Some(3)),
+      SelfReferencingTable.filter(_.id === 12).map(_.parentId).update(Some(4)),
+      SelfReferencingTable.filter(_.id === 13).map(_.parentId).update(Some(5)),
+      SelfReferencingTable.filter(_.id === 14).map(_.parentId).update(Some(6)),
+      SelfReferencingTable.filter(_.id === 15).map(_.parentId).update(Some(7)),
+      SelfReferencingTable.filter(_.id === 16).map(_.parentId).update(Some(15))
     )
   }
 }
