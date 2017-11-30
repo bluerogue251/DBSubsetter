eaa8a2511887018e91223be8e3fb7adf066726ef
Teddy Widom <theodore.widom@gmail.com>
2017-11-30 16:16:53 -0500

    Add cross-schema test

--- src/test/scala/e2e/crossschema/CrossSchemaDDL.scala
+++ src/test/scala/e2e/crossschema/CrossSchemaDDL.scala
@@ -6,7 +6,7 @@ trait CrossSchemaDDL {
   import profile.api._
   import slick.model.ForeignKeyAction
 
-  lazy val schema: profile.SchemaDescription = Schema1Table.schema ++ Schema2Table.schema
+  lazy val schema: profile.SchemaDescription = Schema1Table.schema ++ Schema2Table.schema ++ Schema3Table.schema
 
   /**
     * schema_1_table
@@ -29,7 +29,7 @@ trait CrossSchemaDDL {
   class Schema2Table(_tableTag: Tag) extends profile.api.Table[Schema2TableRow](_tableTag, Some("schema_2"), "schema_2_table") {
     def * = (id, schema1TableId) <> (Schema2TableRow.tupled, Schema2TableRow.unapply)
 
-    val id: Rep[Int] = column[Int]("iD", O.PrimaryKey)
+    val id: Rep[Int] = column[Int]("id", O.PrimaryKey)
     val schema1TableId: Rep[Int] = column[Int]("schema_1_table_id")
     lazy val fk = foreignKey("fkey", schema1TableId, Schema1Table)(_.id, onUpdate = ForeignKeyAction.NoAction, onDelete = ForeignKeyAction.NoAction)
   }
@@ -44,7 +44,7 @@ trait CrossSchemaDDL {
   class Schema3Table(_tableTag: Tag) extends profile.api.Table[Schema3TableRow](_tableTag, Some("schema_3"), "schema_3_table") {
     def * = (id, schema2TableId) <> (Schema3TableRow.tupled, Schema3TableRow.unapply)
 
-    val id: Rep[Int] = column[Int]("iD", O.PrimaryKey)
+    val id: Rep[Int] = column[Int]("id", O.PrimaryKey)
     val schema2TableId: Rep[Int] = column[Int]("schema_2_table_id")
     lazy val fk = foreignKey("fkey", schema2TableId, Schema2Table)(_.id, onUpdate = ForeignKeyAction.NoAction, onDelete = ForeignKeyAction.NoAction)
   }
