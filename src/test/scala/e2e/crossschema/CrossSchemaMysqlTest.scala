d9fbf91c6fc16a9e9414b0a80bed9ca3e930d365
Teddy Widom <theodore.widom@gmail.com>
2017-11-30 16:29:37 -0500

    Add cross-schema test

--- src/test/scala/e2e/crossschema/CrossSchemaMysqlTest.scala
+++ src/test/scala/e2e/crossschema/CrossSchemaMysqlTest.scala
@@ -17,4 +17,22 @@ class CrossSchemaMysqlTest extends AbstractMysqlEndToEndTest with CrossSchemaTes
     s"./util/create_mysql_db.sh schema_3 $originPort".!!
     super.setupDDL()
   }
+
+  override def setupTargetDbs(): Unit = {
+    super.setupTargetDbs()
+
+    s"./util/create_mysql_db.sh schema_1 $targetSingleThreadedPort".!!
+    s"./util/create_mysql_db.sh schema_2 $targetSingleThreadedPort".!!
+    s"./util/create_mysql_db.sh schema_3 $targetSingleThreadedPort".!!
+    s"./util/sync_mysql_origin_to_target.sh schema_1 $originPort $targetSingleThreadedPort".!!
+    s"./util/sync_mysql_origin_to_target.sh schema_2 $originPort $targetSingleThreadedPort".!!
+    s"./util/sync_mysql_origin_to_target.sh schema_3 $originPort $targetSingleThreadedPort".!!
+
+    s"./util/create_mysql_db.sh schema_1 $targetAkkaStreamsPort".!!
+    s"./util/create_mysql_db.sh schema_2 $targetAkkaStreamsPort".!!
+    s"./util/create_mysql_db.sh schema_3 $targetAkkaStreamsPort".!!
+    s"./util/sync_mysql_origin_to_target.sh schema_1 $originPort $targetAkkaStreamsPort".!!
+    s"./util/sync_mysql_origin_to_target.sh schema_2 $originPort $targetAkkaStreamsPort".!!
+    s"./util/sync_mysql_origin_to_target.sh schema_3 $originPort $targetAkkaStreamsPort".!!
+  }
 }
