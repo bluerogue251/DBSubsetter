e114753dda73ee62efaa27f0e20f300d502d7246
Teddy Widom <theodore.widom@gmail.com>
2017-12-13 14:01:48 -0500

    Continue work on Chronicle Queue

--- src/main/scala/trw/dbsubsetter/workflow/TaskQueueReader.scala
+++ src/main/scala/trw/dbsubsetter/workflow/TaskQueueReader.scala
@@ -7,17 +7,17 @@ import net.openhft.chronicle.wire.ValueIn
 class TaskQueueReader(typeList: Array[JDBCType]) {
   def read(in: ValueIn): Array[Any] = handlerFunc(in)
 
-  private val funcs: Array[ValueIn => Any] = typeList.map {
-    case JDBCType.TINYINT | JDBCType.SMALLINT => (in: ValueIn) => in.int16()
-    case JDBCType.INTEGER => (in: ValueIn) => in.int32()
-    case JDBCType.BIGINT => (in: ValueIn) => in.int64()
-    case JDBCType.VARCHAR | JDBCType.CHAR | JDBCType.LONGVARCHAR | JDBCType.NCHAR => (in: ValueIn) => in.text()
-    case other => throw new RuntimeException(s"JDBC Type not yet supported for foreign key column: $other. Please open a GitHub issue for this.")
-  }
-
-  private val headFunc: ValueIn => Any = funcs.head
-
   private val handlerFunc: ValueIn => Array[Any] = {
+    val funcs: Array[ValueIn => Any] = typeList.map {
+      case JDBCType.TINYINT | JDBCType.SMALLINT => (in: ValueIn) => in.int16()
+      case JDBCType.INTEGER => (in: ValueIn) => in.int32()
+      case JDBCType.BIGINT => (in: ValueIn) => in.int64()
+      case JDBCType.VARCHAR | JDBCType.CHAR | JDBCType.LONGVARCHAR | JDBCType.NCHAR => (in: ValueIn) => in.text()
+      case other => throw new RuntimeException(s"JDBC Type not yet supported for foreign key column: $other. Please open a GitHub issue for this.")
+    }
+
+    val headFunc: ValueIn => Any = funcs.head
+
     if (typeList.lengthCompare(1) == 0) {
       (in: ValueIn) => Array[Any](headFunc(in))
     } else {
--- src/main/scala/trw/dbsubsetter/workflow/TaskQueueWriter.scala
+++ src/main/scala/trw/dbsubsetter/workflow/TaskQueueWriter.scala
@@ -15,18 +15,17 @@ class TaskQueueWriter(fkOrdinal: Short, typeList: Seq[JDBCType]) {
     }
   }
 
-  // TODO nest these inside handlerFunc?
-  private val funcs: Array[(ValueOut, Any) => WireOut] = typeList.map {
-    case JDBCType.TINYINT | JDBCType.SMALLINT => (out: ValueOut, fkVal: Any) => out.int16(fkVal.asInstanceOf[Short])
-    case JDBCType.INTEGER => (out: ValueOut, fkVal: Any) => out.int32(fkVal.asInstanceOf[Int])
-    case JDBCType.BIGINT => (out: ValueOut, fkVal: Any) => out.int64(fkVal.asInstanceOf[Long])
-    case JDBCType.VARCHAR | JDBCType.CHAR | JDBCType.LONGVARCHAR | JDBCType.NCHAR => (out: ValueOut, fkVal: Any) => out.text(fkVal.asInstanceOf[String])
-    case other => throw new RuntimeException(s"JDBC Type not yet supported for foreign key column: $other. Please open a GitHub issue for this.")
-  }.toArray
-
-  private val headFunc: (ValueOut, Any) => WireOut = funcs.head
-
   private val handlerFunc: (ValueOut, Array[Any]) => Unit = {
+    val funcs: Array[(ValueOut, Any) => WireOut] = typeList.map {
+      case JDBCType.TINYINT | JDBCType.SMALLINT => (out: ValueOut, fkVal: Any) => out.int16(fkVal.asInstanceOf[Short])
+      case JDBCType.INTEGER => (out: ValueOut, fkVal: Any) => out.int32(fkVal.asInstanceOf[Int])
+      case JDBCType.BIGINT => (out: ValueOut, fkVal: Any) => out.int64(fkVal.asInstanceOf[Long])
+      case JDBCType.VARCHAR | JDBCType.CHAR | JDBCType.LONGVARCHAR | JDBCType.NCHAR => (out: ValueOut, fkVal: Any) => out.text(fkVal.asInstanceOf[String])
+      case other => throw new RuntimeException(s"JDBC Type not yet supported for foreign key column: $other. Please open a GitHub issue for this.")
+    }.toArray
+
+    val headFunc: (ValueOut, Any) => WireOut = funcs.head
+
     if (typeList.lengthCompare(1) == 0) {
       (out, fkValue) => headFunc(out, fkValue.head)
     } else {
