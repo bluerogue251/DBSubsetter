12663a37516dcefa76b8142f229de7249f0fecb6
Teddy Widom <theodore.widom@gmail.com>
2018-12-27 18:41:29 -0500

    Reduce data size by factor of 100 to improve test runtime
    (This is not a load test, so there's no reason to test with lots of data, and it was very slow in SQLServer)

--- src/test/scala/e2e/circulardep/CircularDepDML.scala
+++ src/test/scala/e2e/circulardep/CircularDepDML.scala
@@ -8,11 +8,11 @@ class CircularDepDML(val profile: JdbcProfile) extends CircularDepDDL {
 
   def dbioSeq = {
     val insertStatements = Seq(
-      Grandparents ++= (0 to 1000).map(i => GrandparentsRow(i, None)),
-      Parents ++= (0 to 10009).map(i => ParentsRow(i, i / 10)),
-      Children ++= (0 to 50049).map(i => ChildrenRow(i, i / 5))
+      Grandparents ++= (0 to 10).map(i => GrandparentsRow(i, None)),
+      Parents ++= (0 to 109).map(i => ParentsRow(i, i / 10)),
+      Children ++= (0 to 549).map(i => ChildrenRow(i, i / 5))
     )
-    val updateStatements = (0 to 1000 by 3).map { i =>
+    val updateStatements = (0 to 10 by 3).map { i =>
       Grandparents.filter(_.id === i).map(_.favoriteParentId).update(Some(i * 10))
     }
     val allStatements = insertStatements ++ updateStatements
