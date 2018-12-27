abedfcdcb6668af3606c5a5e2417b545bc791b6b
Teddy Widom <theodore.widom@gmail.com>
2018-12-27 18:25:11 -0500

    Re-enable SQLServer Tests now that docker defaults to overlay2
    See:
    https://github.com/Microsoft/mssql-docker/issues/171
    https://github.com/Microsoft/mssql-docker/issues/181

--- src/test/scala/e2e/circulardep/CircularDepSqlServerTest.scala
+++ src/test/scala/e2e/circulardep/CircularDepSqlServerTest.scala
@@ -1,12 +1,12 @@
-//package e2e.circulardep
-//
-//import e2e.AbstractSqlServerEndToEndTest
-//
-//class CircularDepSqlServerTest extends AbstractSqlServerEndToEndTest with CircularDepTestCases {
-//  override val originPort = 5486
-//  override val programArgs = Array(
-//    "--schemas", "dbo",
-//    "--baseQuery", "dbo.grandparents ::: id % 6 = 0 ::: includeChildren",
-//    "--skipPkStore", "dbo.children"
-//  )
-//}
+package e2e.circulardep
+
+import e2e.AbstractSqlServerEndToEndTest
+
+class CircularDepSqlServerTest extends AbstractSqlServerEndToEndTest with CircularDepTestCases {
+  override val originPort = 5486
+  override val programArgs = Array(
+    "--schemas", "dbo",
+    "--baseQuery", "dbo.grandparents ::: id % 6 = 0 ::: includeChildren",
+    "--skipPkStore", "dbo.children"
+  )
+}
--- src/test/scala/e2e/mixedcase/MixedCaseSqlServerTest.scala
+++ src/test/scala/e2e/mixedcase/MixedCaseSqlServerTest.scala
@@ -1,13 +1,13 @@
-//package e2e.mixedcase
-//
-//import e2e.AbstractSqlServerEndToEndTest
-//
-//class MixedCaseSqlServerTest extends AbstractSqlServerEndToEndTest with MixedCaseTestCases {
-//  override val originPort = 5536
-//  override val programArgs = Array(
-//    "--schemas", "dbo",
-//    "--baseQuery", "dbo.mixed_CASE_table_1 ::: [ID] = 2 ::: includeChildren",
-//    "--skipPkStore", "dbo.mixed_CASE_table_1",
-//    "--skipPkStore", "dbo.mixed_CASE_table_2"
-//  )
-//}
+package e2e.mixedcase
+
+import e2e.AbstractSqlServerEndToEndTest
+
+class MixedCaseSqlServerTest extends AbstractSqlServerEndToEndTest with MixedCaseTestCases {
+  override val originPort = 5536
+  override val programArgs = Array(
+    "--schemas", "dbo",
+    "--baseQuery", "dbo.mixed_CASE_table_1 ::: [ID] = 2 ::: includeChildren",
+    "--skipPkStore", "dbo.mixed_CASE_table_1",
+    "--skipPkStore", "dbo.mixed_CASE_table_2"
+  )
+}
