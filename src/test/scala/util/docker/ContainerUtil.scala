0d3e3ff081595d6f1aa15514d2aa8ea23f64fb38
Teddy Widom <theodore.widom@gmail.com>
2018-12-27 22:18:50 -0500

    Fix typo

--- src/test/scala/util/docker/ContainerUtil.scala
+++ src/test/scala/util/docker/ContainerUtil.scala
@@ -4,7 +4,7 @@ import scala.sys.process._
 
 object ContainerUtil {
   def rm(containerName: String): Unit = {
-    s"docker rm --force --volumes $containerName'".!
+    s"docker rm --force --volumes $containerName".!
   }
 
   def start(containerName: String): Unit = {
