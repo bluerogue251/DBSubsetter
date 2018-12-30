00c44b3b1b1f3699bb0bc83eb3436abeb9715bb5
Teddy Widom <theodore.widom@gmail.com>
2018-12-30 14:28:40 -0500

    Start work on SQL Server

--- src/test/scala/e2e/AbstractSqlServerEndToEndTest.scala
+++ src/test/scala/e2e/AbstractSqlServerEndToEndTest.scala
@@ -4,9 +4,6 @@ import util.db._
 
 import scala.sys.process._
 
-/*
- * Purposely shares a single container between origin and target DBs
-*/
 abstract class AbstractSqlServerEndToEndTest extends AbstractEndToEndTest[SqlServerDatabase] {
   override protected val profile = slick.jdbc.SQLServerProfile
 
@@ -14,9 +11,7 @@ abstract class AbstractSqlServerEndToEndTest extends AbstractEndToEndTest[SqlSer
 
   protected def port: Int
 
-  override protected def startOriginContainer():Unit = {
-    DatabaseContainer.startSqlServer(containers.origin.name, port)
-  }
+  override protected def startOriginContainer():Unit = SharedTestContainers.sqlServer
 
   override protected def startTargetContainers(): Unit = {} // No-op (container is shared with origin)
 
@@ -32,7 +27,7 @@ abstract class AbstractSqlServerEndToEndTest extends AbstractEndToEndTest[SqlSer
   }
 
   override protected def containers: DatabaseContainerSet[SqlServerDatabase] = {
-    val containerName = s"${testName}_sqlserver"
+    val containerName = SharedTestContainers.sqlServer.name
     val originDbName = s"${testName}_origin"
     val targetSingleThreadedDbName = s"${testName}_target_single_threaded"
     val targetAkkaStreamsDbName = s"${testName}_target_akka_streams"
--- src/test/scala/e2e/SharedTestContainers.scala
+++ src/test/scala/e2e/SharedTestContainers.scala
@@ -4,9 +4,10 @@ import util.db._
 import util.docker.ContainerUtil
 
 object SharedTestContainers {
+  private val dbName = "do-not-use"
+
   lazy val postgres: DatabaseContainer[PostgreSQLDatabase] = {
     val containerName = "e2e_postgres"
-    val dbName = "postgres"
     val port = 5497
     DatabaseContainer.startPostgreSQL(containerName, port)
     val db = new PostgreSQLDatabase(dbName, port)
@@ -21,7 +22,6 @@ object SharedTestContainers {
 
   lazy val mysql: DatabaseContainer[MySqlDatabase] = {
     val containerName = "e2e_mysql"
-    val dbName = "mysql"
     val port = 5498
     DatabaseContainer.startMySql(containerName, port)
     val db = new MySqlDatabase(dbName, port)
@@ -32,5 +32,19 @@ object SharedTestContainers {
     sys.addShutdownHook(ContainerUtil.rm(containerName))
 
     new MySqlContainer(containerName, db)
+  }
+
+  lazy val sqlServer: DatabaseContainer[SqlServerDatabase] = {
+    val containerName = "e2e_sql_server"
+    val port = 5499
+    DatabaseContainer.startSqlServer(containerName, port)
+    val db = new SqlServerDatabase(dbName, port)
+
+    /*
+     * Remove container on JVM shutdown
+     */
+    sys.addShutdownHook(ContainerUtil.rm(containerName))
+
+    new SqlServerContainer(containerName, db)
   }
 }
