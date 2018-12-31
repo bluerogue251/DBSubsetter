f0962cbc6a2b31dfd56d3336897d6b2cefcbb529
Teddy Widom <theodore.widom@gmail.com>
2018-12-31 00:17:29 -0500

    Fix comment

--- src/test/scala/load/physics/copy_domain_data_postgres.sh
+++ src/test/scala/load/physics/copy_domain_data_postgres.sh
@@ -4,7 +4,6 @@
 origin_container=$1
 target_container=$2
 
-# Single Threaded Target
 docker exec ${origin_container} psql --user postgres --dbname physics -c 'copy particle_domain to stdout' | docker exec -i ${target_container} psql --user postgres --dbname physics -c 'copy particle_domain from stdin'
 docker exec ${origin_container} psql --user postgres --dbname physics -c 'copy quantum_domain to stdout' | docker exec -i ${target_container} psql --user postgres --dbname physics -c 'copy quantum_domain from stdin'
 docker exec ${origin_container} psql --user postgres --dbname physics -c 'copy gravitational_wave_domain to stdout' | docker exec -i ${target_container} psql --user postgres --dbname physics -c 'copy gravitational_wave_domain from stdin'
