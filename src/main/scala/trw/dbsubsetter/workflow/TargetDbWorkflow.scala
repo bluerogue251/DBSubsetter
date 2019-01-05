627410df77b2212ee1eab4283dc807aeed2f411e
Teddy Widom <theodore.widom@gmail.com>
2019-01-05 01:24:27 -0500

    Throw exception on unexpected target db insert result

--- src/main/scala/trw/dbsubsetter/workflow/TargetDbWorkflow.scala
+++ src/main/scala/trw/dbsubsetter/workflow/TargetDbWorkflow.scala
@@ -1,16 +1,22 @@
 package trw.dbsubsetter.workflow
 
 import trw.dbsubsetter.config.Config
-import trw.dbsubsetter.db.{DbAccessFactory, SchemaInfo}
+import trw.dbsubsetter.db.{DbAccessFactory, Row, SchemaInfo}
 
 
 class TargetDbWorkflow(config: Config, schemaInfo: SchemaInfo, dbAccessFactory: DbAccessFactory) {
+
   private[this] val dbAccess = dbAccessFactory.buildTargetDbAccess()
 
-  // The fact that a row still needs parent tasks means this is the first time we've seen it
-  // I.e. it has not yet been added to the target db
   def process(request: PksAdded): TargetDbInsertResult = {
+    // The fact that a row still needs parent tasks means this is the first time we've seen it and
+    // it therefore should be be added to the target db. Use IndexedSeq type signature to ensure O(1) .length call
+    val rowsToInsert: IndexedSeq[Row] = request.rowsNeedingParentTasks
     val rowsInserted = dbAccess.insertRows(request.table, request.rowsNeedingParentTasks)
+    if (rowsInserted != rowsToInsert.length) {
+      val message = s"Unexpected number of rows inserted into target DB. Table: ${request.table}. Expected ${rowsToInsert.length}. Actual $rowsInserted."
+      throw new RuntimeException(message)
+    }
     TargetDbInsertResult(request.table, rowsInserted)
   }
 }
