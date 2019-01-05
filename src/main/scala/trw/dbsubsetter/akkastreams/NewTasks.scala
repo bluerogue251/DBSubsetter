058efd8e608fc4be37deeb9df56d32b17e9a3d71
Teddy Widom <theodore.widom@gmail.com>
2019-01-05 16:56:09 -0500

    Compiling

--- src/main/scala/trw/dbsubsetter/akkastreams/NewTasks.scala
+++ src/main/scala/trw/dbsubsetter/akkastreams/NewTasks.scala
@@ -2,11 +2,11 @@ package trw.dbsubsetter.akkastreams
 
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
-import trw.dbsubsetter.db.{ForeignKey, SchemaInfo}
+import trw.dbsubsetter.db.SchemaInfo
 import trw.dbsubsetter.workflow._
 
 object NewTasks {
   def flow(sch: SchemaInfo): Flow[PksAdded, NewTasks, NotUsed] = {
-    Flow[PksAdded].map[Map[(ForeignKey, Boolean), Array[Any]]](pksAdded => NewFkTaskWorkflow.process(pksAdded, sch))
+    Flow[PksAdded].map(pksAdded => NewFkTaskWorkflow.process(pksAdded, sch))
   }
 }
