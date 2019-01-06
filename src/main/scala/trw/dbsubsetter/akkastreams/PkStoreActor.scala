7190055e152bfbc9069118cac01aee3375a3f089
Teddy Widom <theodore.widom@gmail.com>
2019-01-05 19:20:54 -0500

    Extract values

--- src/main/scala/trw/dbsubsetter/akkastreams/PkStoreActor.scala
+++ src/main/scala/trw/dbsubsetter/akkastreams/PkStoreActor.scala
@@ -14,15 +14,13 @@ private[this] class PkStoreActor(schemaInfo: SchemaInfo) extends Actor {
 
   override def receive: Receive = {
     // If it's a FkTask, then we are being asked to pre-check to make sure we haven't done it already
-    case task: FetchParentTask =>
-      val alreadySeen: Boolean = pkStore.alreadySeen(task.table, task.fkValue)
+    case task @ FetchParentTask(table, _, fkValue) =>
+      val alreadySeen: Boolean = pkStore.alreadySeen(table, fkValue)
       val response: PkQueryResult = if (alreadySeen) AlreadySeen else NotAlreadySeen(task)
       sender() ! response
     // If it's an OriginDbResult, then we are being asked to add the new primary key values to the PkStore
     case req: OriginDbResult =>
       sender() ! pkStoreWorkflow.add(req)
-    case other =>
-      throw new RuntimeException(s"Cannot handle $other")
   }
 }
 
