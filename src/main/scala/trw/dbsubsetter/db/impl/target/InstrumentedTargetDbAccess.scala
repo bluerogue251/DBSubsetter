16e648641dff45e23f04f81bd68d2d4868251517
Teddy Widom <theodore.widom@gmail.com>
2019-09-28 20:07:06 -0400

    Configure Drone CI (#80)
    * Add in CI capabilities to help build and test the code before PR merges.
    * Currently will only run the unit and integration tests -- e2e tests should hopefully come soon after this. Load tests will be quite challenging, but will hopefully come some day.
    * Adding in CI has already caught a major problem -- the .gitignore file excluded all folders called `target`, including code I wrote myself. This was not visible to me, since the files that I put in that directory stayed on my local machine and were available there, although they never made it into GitHub. Trying to run the code on CI showed those files to be missing. This PR adds those files officially into Git and updates the .gitignore rules accordingly.

---
+++ src/main/scala/trw/dbsubsetter/db/impl/target/InstrumentedTargetDbAccess.scala
@@ -0,0 +1,17 @@
+package trw.dbsubsetter.db.impl.target
+
+import io.prometheus.client.{Counter, Histogram}
+import trw.dbsubsetter.db.{Row, Table, TargetDbAccess}
+import trw.dbsubsetter.metrics.Metrics
+
+private[db] class InstrumentedTargetDbAccess(delegatee: TargetDbAccess) extends TargetDbAccess {
+
+  private[this] val histogram: Histogram = Metrics.TargetDbInsertsHistogram
+
+  private[this] val rowCounter: Counter = Metrics.TargetDbRowsInserted
+
+  override def insertRows(table: Table, rows: Vector[Row]): Int = {
+    rowCounter.inc(rows.size)
+    histogram.time(() => delegatee.insertRows(table, rows))
+  }
+}
---
+++ src/main/scala/trw/dbsubsetter/db/impl/target/TargetDbAccessImpl.scala
@@ -0,0 +1,32 @@
+package trw.dbsubsetter.db.impl.target
+
+import java.sql.Connection
+
+import trw.dbsubsetter.db.impl.connection.ConnectionFactory
+import trw.dbsubsetter.db.{Row, SchemaInfo, Sql, Table, TargetDbAccess}
+
+private[db] class TargetDbAccessImpl(connStr: String, sch: SchemaInfo, connectionFactory: ConnectionFactory) extends TargetDbAccess {
+
+  private[this] val connection: Connection =
+    connectionFactory.getConnectionWithWritePrivileges(connStr)
+
+  private[this] val statements = Sql.preparedInsertStatementStrings(sch).map { case (table, sqlStr) =>
+    table -> connection.prepareStatement(sqlStr)
+  }
+
+  // TODO reconsider Int return type if we just return 1 -- it's confusing
+  override def insertRows(table: Table, rows: Vector[Row]): Int = {
+
+    val stmt = statements(table)
+    val cols = sch.colsByTableOrdered(table).size
+
+    rows.foreach { row =>
+      (1 to cols).foreach(i => stmt.setObject(i, row(i - 1)))
+      stmt.addBatch()
+    }
+
+    stmt.executeBatch()
+    1
+  }
+
+}
