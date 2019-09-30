81bcc471597da2928da7b4b1939880c89252060f
Teddy Widom <theodore.widom@gmail.com>
2019-09-29 20:54:28 -0400

    Feeling stuck

--- src/test/scala/e2e/AbstractMysqlEndToEndTest.scala
+++ src/test/scala/e2e/AbstractMysqlEndToEndTest.scala
@@ -71,6 +71,7 @@ abstract class AbstractMysqlEndToEndTest extends AbstractEndToEndTest[MySqlDatab
 object MysqlEndToEndTestUtil {
   def createSchemas(db: MySqlDatabase, schemas: List[String]): Unit = {
     schemas.foreach(schema => {
+      s"""mysql --host ${db.host} --port ${db.port} --user root -e "drop database if exists $schema"""".!!
       s"""mysql --host ${db.host} --port ${db.port} --user root -e "create database $schema"""".!!
     })
   }
