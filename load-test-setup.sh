5aec1efb8abe8891f525afaed4f52bf07275d87a
Teddy Widom <theodore.widom@gmail.com>
2019-10-05 16:47:58 -0400

    Incorporate physics_db into AWS setup

--- load-test-setup.sh
+++ load-test-setup.sh
@@ -2,35 +2,51 @@
 
 set -eou pipefail
 
-sudo snap install docker # in the future try to lock this down to docker version 18.06.1-ce, build e68fc7a
+# Install docker (in the future try to lock this down to version 18.06.1-ce)
+sudo snap install docker
+
+# Install JRE
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo apt install -y openjdk-8-jre-headless
 
 mkdir --parents /home/ubuntu/pgdata/schooldb/origin
+mkdir --parents /home/ubuntu/tmp-data
 
 sudo docker run \
   --detach \
-  --name pg_school_db_origin \
+  --name pg_origin \
   --volume /home/ubuntu/pgdata/schooldb/origin:/var/lib/postgresql/data \
+  --volume /home/ubuntu/tmp-data:/tmp-data \
   -p 5432:5432 \
   postgres:9.6.3 \
   postgres -c 'max_connections=15'
 
 sudo docker run \
   --detach \
-  --name pg_school_db_target \
+  --name pg_target \
   -p 5433:5432 \
   postgres:9.6.3 \
   postgres -c 'max_connections=15'
 
-sudo docker exec pg_school_db_origin createdb --user postgres school_db
-sudo docker exec pg_school_db_target createdb --user postgres school_db
+sudo docker exec pg_origin createdb --user postgres school_db
+sudo docker exec pg_target createdb --user postgres school_db
 
-DUMP_URL="https://s3.amazonaws.com/db-subsetter/load-test/school-db/pgdump.sql.gz"
-wget -q -O - "${DUMP_URL}" | gunzip | sudo docker exec --interactive --env pg_school_db_origin psql --user postgres --dbname school_db
-sudo docker exec pg_school_db_origin psql --user postgres --dbname school_db -c "VACUUM ANALYZE"
+sudo docker exec pg_origin createdb --user postgres physics_db
+sudo docker exec pg_target createdb --user postgres physics_db
 
-sudo docker exec pg_school_db_origin pg_dump --user postgres --dbname school_db --section pre-data | \
-  sudo docker exec --interactive pg_school_db_target psql --user postgres --dbname school_db
+# Only do this block if we are populating the DBs from scratch
+SCHOOL_DB_DUMP_URL="https://s3.amazonaws.com/db-subsetter/load-test/school-db/pgdump.sql.gz"
+wget -q -O - "${SCHOOL_DB_DUMP_URL}" | gunzip | sudo docker exec --interactive pg_origin psql --user postgres --dbname school_db
 
-# Install JRE
-sudo add-apt-repository -y ppa:openjdk-r/ppa
-sudo apt install -y openjdk-8-jre-headless
+wget -O /home/ubuntu/tmp-data/physics-db-dump.tar "https://s3.amazonaws.com/db-subsetter/load-test/physics-db/physics-db-dump.tar"
+sudo tar -xvf /home/ubuntu/tmp-data/physics-db-dump.tar --directory /home/ubuntu/tmp-data/
+sudo docker exec pg_origin pg_restore --jobs 8 --user postgres --dbname physics_db /tmp-data/physics-db-dump.tar
+
+sudo docker exec pg_origin psql --user postgres --dbname school_db -c "VACUUM ANALYZE"
+sudo docker exec pg_origin psql --user postgres --dbname physics_db -c "VACUUM ANALYZE"
+
+sudo docker exec pg_origin pg_dump --user postgres --dbname school_db --section pre-data | \
+  sudo docker exec --interactive pg_target psql --user postgres --dbname school_db
+
+sudo docker exec pg_origin pg_dump --user postgres --dbname physics_db --section pre-data | \
+  sudo docker exec --interactive pg_target psql --user postgres --dbname physics_db
