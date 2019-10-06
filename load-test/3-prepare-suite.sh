4fd29d98973d16631240139b509f475e86382919
Teddy Widom <theodore.widom@gmail.com>
2019-10-06 15:48:01 -0400

    tweak

--- load-test/3-prepare-suite.sh
+++ load-test/3-prepare-suite.sh
@@ -6,6 +6,6 @@ set -eou pipefail
 DB_SUBSETTER_JAR_URL="https://s3.amazonaws.com/db-subsetter/load-test/jars/DBSubsetter-assembly-f91e64d0d622aeebf44f217e365f35ac990fd534.jar"
 wget -O /home/ubuntu/DBSubsetter.jar "${DB_SUBSETTER_JAR_URL}"
 
-sudo docker start --detach pg_origin
-sudo docker start --detach pg_target
-sudo docker start --detach prometheus
+sudo docker start pg_origin
+sudo docker start pg_target
+sudo docker start prometheus
