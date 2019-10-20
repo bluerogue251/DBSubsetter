90c2cae025a51b0a65e90e8c931079e9c7619e82
Teddy Widom <theodore.widom@gmail.com>
2019-10-19 20:21:54 -0400

    Revert

--- src/main/scala/trw/dbsubsetter/akkastreams/TargetDb.scala
+++ src/main/scala/trw/dbsubsetter/akkastreams/TargetDb.scala
@@ -1,16 +1,14 @@
 package trw.dbsubsetter.akkastreams
 
-import akka.Done
-import akka.stream.scaladsl.Sink
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
 import trw.dbsubsetter.config.Config
 import trw.dbsubsetter.db.{DbAccessFactory, SchemaInfo}
 import trw.dbsubsetter.workflow._
 
-import scala.concurrent.Future
-
 private[akkastreams] object TargetDb {
-  def insert(config: Config, schemaInfo: SchemaInfo, dbAccessFactory: DbAccessFactory): Sink[PksAdded, Future[Done]] = {
+  def insert(config: Config, schemaInfo: SchemaInfo, dbAccessFactory: DbAccessFactory): Flow[PksAdded, Unit, NotUsed] = {
     val dbWorkflow = new TargetDbWorkflow(config, schemaInfo, dbAccessFactory)
-    Sink.foreach[PksAdded](dbWorkflow.process)
+    Flow[PksAdded].map(dbWorkflow.process)
   }
 }
