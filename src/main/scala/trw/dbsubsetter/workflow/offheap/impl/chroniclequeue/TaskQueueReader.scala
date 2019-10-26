769251de4078a1dded0dea6386f9a7df77970ef6
Teddy Widom <theodore.widom@gmail.com>
2019-10-26 17:49:47 -0400

    Chronicle queue refactors [Part 2] (#104)
    Part 2 of a preparatory refactor which should make it easier to eventually accomplish https://github.com/bluerogue251/DBSubsetter/issues/33
    
    Cleans up chronicle queue code a bit to make it easier to extend later to include queueing up primary key values for consumption by the data copying feature. Specifically, extract helpers which map from a column type to the chronicle queue function that can write or read a single value of that column type. This will then be leveraged for writing and reading primary key column data.

--- src/main/scala/trw/dbsubsetter/workflow/offheap/impl/chroniclequeue/TaskQueueReader.scala
+++ src/main/scala/trw/dbsubsetter/workflow/offheap/impl/chroniclequeue/TaskQueueReader.scala
@@ -1,43 +1,22 @@
 package trw.dbsubsetter.workflow.offheap.impl.chroniclequeue
 
 import net.openhft.chronicle.wire.ValueIn
-import trw.dbsubsetter.db.ColumnTypes
 import trw.dbsubsetter.db.ColumnTypes.ColumnType
 
+
 private[offheap] final class TaskQueueReader(columnTypes: Seq[ColumnType]) {
-
-  def read(in: ValueIn): Any = {
-    valueReader(in)
-  }
-
-  private val valueReader: ValueIn => Any = {
+  private[this] val valueReader: ValueIn => Any = {
     val funcs: Seq[ValueIn => Any] =
-      columnTypes.map {
-        case ColumnTypes.Short =>
-          (in: ValueIn) => in.int16()
-        case ColumnTypes.Int =>
-          (in: ValueIn) => in.int32()
-        case ColumnTypes.Long =>
-          (in: ValueIn) => in.int64()
-        case ColumnTypes.BigInteger =>
-          (in: ValueIn) => in.`object`()
-        case ColumnTypes.String =>
-          (in: ValueIn) => in.text()
-        case ColumnTypes.ByteArray =>
-          (in: ValueIn) => in.bytes()
-        case ColumnTypes.Uuid =>
-          (in: ValueIn) => in.uuid()
-        case ColumnTypes.Unknown(description) =>
-          val errorMessage =
-            s"Column type not yet fully supported: $description. " +
-              "Please open a GitHub issue and we will try to address it promptly."
-          throw new RuntimeException(errorMessage)
-      }
+      columnTypes.map(ChronicleQueueFunctions.singleValueRead)
 
     if (columnTypes.size == 1) {
       in: ValueIn => funcs.head(in)
     } else {
       in: ValueIn => funcs.toArray.map(f => f(in))
     }
+  }
+
+  def read(in: ValueIn): Any = {
+    valueReader(in)
   }
 }
