c729e17a08f379a29b4331d67a07451ed7bfe6e3
Teddy Widom <theodore.widom@gmail.com>
2019-10-26 12:21:21 -0400

    Start work on batched data copying

--- src/main/scala/trw/dbsubsetter/db/Constants.scala
+++ src/main/scala/trw/dbsubsetter/db/Constants.scala
@@ -2,5 +2,6 @@ package trw.dbsubsetter.db
 
 object Constants {
 
-  val dataCopyBatchSizes: Seq[Int] = Seq(1, 2, 4, 16, 256, 1024, 2048)
+  // Must be in descending order
+  val dataCopyBatchSizes: Seq[Int] = Seq(2048, 1024, 256, 16, 4, 2, 1)
 }
--- src/main/scala/trw/dbsubsetter/util/BatchingUtil.scala
+++ src/main/scala/trw/dbsubsetter/util/BatchingUtil.scala
@@ -1,18 +1,44 @@
 package trw.dbsubsetter.util
 
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
+
 object BatchingUtil {
 
   /**
-    * @param elements An unbatched sequence of elements
-    * @param batchSizes Desired batch sizes; must be provided in ascending order
+    * @param elements   An unbatched sequence of elements
+    * @param batchSizes Desired batch sizes; must be provided in descending order
     * @return The original elements in as few batches as possible, with each batch size
     *         guaranteed to be included in the supplied list of desired batch sizes.
     */
   def batch[T](elements: Seq[T], batchSizes: Seq[Short]): Seq[Seq[T]] = {
-    if (elements.isEmpty || batchSizes.isEmpty || batchSizes.head != 1) {
+    if (elements.isEmpty || batchSizes.isEmpty) {
       throw new IllegalArgumentException()
     }
 
-    ???
+    batchInternal(elements, batchSizes, ArrayBuffer[Seq[T]]())
+  }
+
+  @tailrec
+  private[this] def batchInternal[T](elems: Seq[T], batchSizes: Seq[Short], accum: ArrayBuffer[Seq[T]]): Seq[Seq[T]] = {
+    if (elems.isEmpty) {
+      accum
+    } else if (batchSizes.isEmpty) {
+      throw new RuntimeException("Failed to batch elements")
+    } else {
+      val currentBatchSize: Short = batchSizes.head
+      val newBatches: IndexedSeq[Seq[T]] = elems.grouped(currentBatchSize).toIndexedSeq
+      for (i <- 0 until newBatches.length - 1) {
+        val definitelyWholeBatch: Seq[T] = newBatches(i)
+        accum += definitelyWholeBatch
+      }
+      val batchOfUnknownSize = newBatches.last
+      if (batchOfUnknownSize.size == currentBatchSize) {
+        accum += batchOfUnknownSize
+        batchInternal(Seq.empty, Seq.empty, accum)
+      } else {
+        batchInternal(batchOfUnknownSize, batchSizes.tail, accum)
+      }
+    }
   }
 }
