b0a729b501cfca2602201162769d7505bfbd6be2
Teddy Widom <theodore.widom@gmail.com>
2019-10-26 12:30:21 -0400

    Refactor batching util

--- src/main/scala/trw/dbsubsetter/util/BatchingUtil.scala
+++ src/main/scala/trw/dbsubsetter/util/BatchingUtil.scala
@@ -23,21 +23,14 @@ object BatchingUtil {
   private[this] def batchInternal[T](elems: Seq[T], batchSizes: Seq[Short], accum: ArrayBuffer[Seq[T]]): Seq[Seq[T]] = {
     if (elems.isEmpty) {
       accum
-    } else if (batchSizes.isEmpty) {
-      throw new RuntimeException("Failed to batch elements")
     } else {
       val currentBatchSize: Short = batchSizes.head
-      val newBatches: IndexedSeq[Seq[T]] = elems.grouped(currentBatchSize).toIndexedSeq
-      for (i <- 0 until newBatches.length - 1) {
-        val definitelyWholeBatch: Seq[T] = newBatches(i)
-        accum += definitelyWholeBatch
-      }
-      val batchOfUnknownSize = newBatches.last
-      if (batchOfUnknownSize.size == currentBatchSize) {
-        accum += batchOfUnknownSize
-        batchInternal(Seq.empty, Seq.empty, accum)
+      val (currentBatch, remainingElems): (Seq[T], Seq[T]) = elems.splitAt(currentBatchSize)
+      if (currentBatch.size < currentBatchSize) {
+        batchInternal(elems, batchSizes.tail, accum)
       } else {
-        batchInternal(batchOfUnknownSize, batchSizes.tail, accum)
+        accum += currentBatch
+        batchInternal(remainingElems, batchSizes, accum)
       }
     }
   }
