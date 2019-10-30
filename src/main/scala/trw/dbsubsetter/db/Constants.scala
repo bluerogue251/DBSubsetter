fbde28dc587a131df73085d215e57310c7606cc3
Teddy Widom <theodore.widom@gmail.com>
2019-10-29 22:03:57 -0400

    Tweaks

--- src/main/scala/trw/dbsubsetter/db/Constants.scala
+++ src/main/scala/trw/dbsubsetter/db/Constants.scala
@@ -4,5 +4,5 @@ object Constants {
 
   // Must be in descending order
   // TODO: investigate the case of a primary key with so many columns that we may run into too many placeholders for a prepared statement.
-  val dataCopyBatchSizes: Seq[Short] = Seq(1000, 512, 256, 16, 4, 2, 1)
+  val dataCopyBatchSizes: Seq[Short] = Seq(1024, 512, 256, 16, 4, 2, 1)
 }
