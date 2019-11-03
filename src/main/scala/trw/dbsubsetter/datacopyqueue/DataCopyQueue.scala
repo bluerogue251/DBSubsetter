c813815181da74fe9d0f6e8a385c3c692d380c91
Teddy Widom <theodore.widom@gmail.com>
2019-11-03 08:50:50 -0500

    Remove BatchingUtil (#128)
    Contributes towards https://github.com/bluerogue251/DBSubsetter/issues/102
    
    Cleanup work after https://github.com/bluerogue251/DBSubsetter/pull/127
    
    * Remove BatchingUtil which was rendered unnecessary
    * Adjust expected load test time to account for new implementation

--- src/main/scala/trw/dbsubsetter/datacopyqueue/DataCopyQueue.scala
+++ src/main/scala/trw/dbsubsetter/datacopyqueue/DataCopyQueue.scala
@@ -4,7 +4,14 @@ import trw.dbsubsetter.workflow.{DataCopyTask, PksAdded}
 
 
 trait DataCopyQueue {
+
   def enqueue(pksAdded: PksAdded): Unit
+
+  /**
+    * @return An optional DataCopyTask. The number of pkValues contained in this DataCopyTask is guaranteed to be
+    *         contained in the list of standard batch sizes declared in Constants.dataCopyBatchSizes
+    */
   def dequeue(): Option[DataCopyTask]
+
   def isEmpty(): Boolean
 }
--- src/main/scala/trw/dbsubsetter/workflow/TargetDbWorkflow.scala
+++ src/main/scala/trw/dbsubsetter/workflow/TargetDbWorkflow.scala
@@ -1,29 +1,28 @@
 package trw.dbsubsetter.workflow
 
 import trw.dbsubsetter.db.{Constants, DbAccessFactory, PrimaryKeyValue, Row}
-import trw.dbsubsetter.util.BatchingUtil
 
 
-// TODO rename this to something more along the lines of "Data Copy Workflow" (as opposed to "Key Query Workflow")
+/*
+ * TODO rename this to something more along the lines of "Data Copy Workflow" (as opposed to "Key Query Workflow"),
+ *  since it involves both the origin and target dbs, not just the target db.
+ */
 final class TargetDbWorkflow(dbAccessFactory: DbAccessFactory) {
 
   private[this] val originDbAccess = dbAccessFactory.buildOriginDbAccess()
 
   private[this] val targetDbAccess = dbAccessFactory.buildTargetDbAccess()
 
-  def process(request: DataCopyTask): Unit = {
-    val pkValues: Seq[PrimaryKeyValue] = request.pkValues
+  def process(dataCopyTask: DataCopyTask): Unit = {
+    val pkValues: Seq[PrimaryKeyValue] = dataCopyTask.pkValues
 
-    if (pkValues.nonEmpty) {
-      val batchedPrimaryKeyValues: Seq[Seq[PrimaryKeyValue]] =
-        BatchingUtil.batch(pkValues, Constants.dataCopyBatchSizes)
-
-      batchedPrimaryKeyValues.foreach(primaryKeyValueBatch => {
-        val rowsToInsert: Vector[Row] =
-          originDbAccess.getRowsFromPrimaryKeyValues(request.table, primaryKeyValueBatch)
-
-        targetDbAccess.insertRows(request.table, rowsToInsert)
-      })
+    if (!Constants.dataCopyBatchSizes.contains(pkValues.size)) {
+      throw new IllegalArgumentException(s"Unsupported data copy batch size: ${pkValues.size}")
     }
+
+    val rowsToInsert: Vector[Row] =
+      originDbAccess.getRowsFromPrimaryKeyValues(dataCopyTask.table, pkValues)
+
+    targetDbAccess.insertRows(dataCopyTask.table, rowsToInsert)
   }
 }
