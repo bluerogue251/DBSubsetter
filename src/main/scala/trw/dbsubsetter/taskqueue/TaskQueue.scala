7dabcc5b43184058f14116df9cc25ad622ff6e40
Teddy Widom <theodore.widom@gmail.com>
2019-11-03 08:31:24 -0500

    Use off heap queues in single threaded mode (#127)
    Contributes towards https://github.com/bluerogue251/DBSubsetter/issues/102

--- src/main/scala/trw/dbsubsetter/taskqueue/TaskQueue.scala
+++
@@ -1,9 +0,0 @@
-package trw.dbsubsetter.taskqueue
-
-import trw.dbsubsetter.workflow.OriginDbRequest
-
-trait TaskQueue {
-  def nonEmpty: Boolean
-  def enqueue(tasks: Seq[OriginDbRequest]): Unit
-  def dequeue(): OriginDbRequest
-}
