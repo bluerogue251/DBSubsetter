b0d6d6dbd7a02fbce084023283462ec8382c05b6
Teddy Widom <theodore.widom@gmail.com>
2019-12-30 19:32:37 -0500

    Rename to match convention

--- src/test/scala/e2e/compositekeys/CompositeKeysDDL.scala
+++ src/test/scala/e2e/compositekeys/CompositeKeysDDL.scala
@@ -13,7 +13,7 @@ class CompositeKeysDDL(val profile: JdbcProfile) {
     */
   case class ParentTableRow(idOne: Int, idTwo: Int)
 
-  class ParentTableDefinition(_tableTag: Tag) extends profile.api.Table[ParentTableRow](_tableTag, "parents") {
+  class ParentTable(_tableTag: Tag) extends profile.api.Table[ParentTableRow](_tableTag, "parents") {
     def * = (idOne, idTwo) <> (ParentTableRow.tupled, ParentTableRow.unapply)
 
     val idOne: Rep[Int] = column[Int]("id_one")
@@ -21,21 +21,21 @@ class CompositeKeysDDL(val profile: JdbcProfile) {
     val pk: PrimaryKey = primaryKey("composite_pk", (idOne, idTwo))
   }
 
-  lazy val ParentTable = new TableQuery(tag => new ParentTableDefinition(tag))
+  lazy val ParentTable = new TableQuery(tag => new ParentTable(tag))
 
   /**
     * children
     */
   case class ChildTableRow(id: Int, parentIdOne: Int, parentIdTwo: Int)
 
-  class ChildTableDefinition(_tableTag: Tag) extends profile.api.Table[ChildTableRow](_tableTag, "children") {
+  class ChildTable(_tableTag: Tag) extends profile.api.Table[ChildTableRow](_tableTag, "children") {
     def * = (id, parentIdOne, parentIdTwo) <> (ChildTableRow.tupled, ChildTableRow.unapply)
 
     val id: Rep[Int] = column[Int]("id", O.PrimaryKey)
     val parentIdOne: Rep[Int] = column[Int]("parent_id_one")
     val parentIdTwo: Rep[Int] = column[Int]("parent_id_two")
-    lazy val fk = foreignKey("foreign_key", (parentIdOne, parentIdTwo), ParentTable)((pt: ParentTableDefinition) => (pt.idOne, pt.idTwo))
+    lazy val fk = foreignKey("foreign_key", (parentIdOne, parentIdTwo), ParentTable)(t => (t.idOne, t.idTwo))
   }
 
-  lazy val ChildTable = new TableQuery(tag => new ChildTableDefinition(tag))
+  lazy val ChildTable = new TableQuery(tag => new ChildTable(tag))
 }
