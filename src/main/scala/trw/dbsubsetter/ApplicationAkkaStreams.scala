950e8a3533648d5e9ed06e1f3e543d77d6f1bf77
Teddy Widom <theodore.widom@gmail.com>
2019-12-31 19:17:35 -0500

    Attempt to optimize JVM Garbage Collection

--- src/main/scala/trw/dbsubsetter/ApplicationAkkaStreams.scala
+++ src/main/scala/trw/dbsubsetter/ApplicationAkkaStreams.scala
@@ -1,7 +1,7 @@
 package trw.dbsubsetter
 
 import akka.Done
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.{ActorRef, ActorSystem, PoisonPill}
 import akka.stream.ActorMaterializer
 import trw.dbsubsetter.akkastreams.{DataCopyGraphFactory, KeyQueryGraphFactory, PkStoreActor}
 import trw.dbsubsetter.config.Config
@@ -19,27 +19,40 @@ object ApplicationAkkaStreams {
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val ec: ExecutionContext = system.dispatcher
 
-    val pkStore: ActorRef = system.actorOf(PkStoreActor.props(config, schemaInfo))
     val dbAccessFactory: DbAccessFactory = new DbAccessFactory(config, schemaInfo)
-    val fkTaskCreationWorkflow: FkTaskCreationWorkflow = new FkTaskCreationWorkflow(schemaInfo)
-    val fkTaskQueue: ForeignKeyTaskQueue = ForeignKeyTaskQueueFactory.build(config, schemaInfo)
     val dataCopyQueue: DataCopyQueue = DataCopyQueueFactory.buildDataCopyQueue(config, schemaInfo)
 
-    val keyQueryPhase: Future[Done] =
-      KeyQueryGraphFactory
-        .build(config, schemaInfo, baseQueries, pkStore, dbAccessFactory, fkTaskCreationWorkflow, fkTaskQueue, dataCopyQueue)
-        .run()
+    // Encapsulate in method so as to encourage JVM to garbage-collect the Primary Key Store as early as possible
+    def runKeyQueryPhase(): Unit = {
+      val pkStore: ActorRef = system.actorOf(PkStoreActor.props(config, schemaInfo))
+      val fkTaskCreationWorkflow: FkTaskCreationWorkflow = new FkTaskCreationWorkflow(schemaInfo)
+      val fkTaskQueue: ForeignKeyTaskQueue = ForeignKeyTaskQueueFactory.build(config, schemaInfo)
 
-    // Wait for the key query phase to complete. Use `result` rather than `ready` to ensure an exception is thrown on failure.
-    Await.result(keyQueryPhase, Duration.Inf)
+      val keyQueryPhase: Future[Done] =
+        KeyQueryGraphFactory
+          .build(config, schemaInfo, baseQueries, pkStore, dbAccessFactory, fkTaskCreationWorkflow, fkTaskQueue, dataCopyQueue)
+          .run()
 
-    val dataCopyPhase: Future[Done] =
-      DataCopyGraphFactory
-        .build(config, schemaInfo, dbAccessFactory, dataCopyQueue)
-        .run()
+      // Wait for the key query phase to complete. Use `result` rather than `ready` to ensure an exception is thrown on failure.
+      Await.result(keyQueryPhase, Duration.Inf)
 
-    // Wait for the data copy phase to complete. Use `result` rather than `ready` to ensure an exception is thrown on failure.
-    Await.result(dataCopyPhase, Duration.Inf)
+      // Explicitly remove the actor from the actor system to allow for JVM Garbage Collection
+      pkStore.tell(PoisonPill, ActorRef.noSender)
+    }
+
+    // Encapsulate in method for consistency with runKeyQueryPhase()
+    def runDataCopyPhase(): Unit = {
+      val dataCopyPhase: Future[Done] =
+        DataCopyGraphFactory
+          .build(config, schemaInfo, dbAccessFactory, dataCopyQueue)
+          .run()
+
+      // Wait for the data copy phase to complete. Use `result` rather than `ready` to ensure an exception is thrown on failure.
+      Await.result(dataCopyPhase, Duration.Inf)
+    }
+
+    runKeyQueryPhase()
+    runDataCopyPhase()
 
     // Clean up after successful subsetting run
     dbAccessFactory.closeAllConnections()
