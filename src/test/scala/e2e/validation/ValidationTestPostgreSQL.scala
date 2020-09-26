351d8b42508069e2c9a1306c5aa70e1ecffceb2d
Teddy Widom <theodore.widom@gmail.com>
2020-09-26 19:49:28 -0400

    Back out of test changes

--- src/test/scala/e2e/validation/ValidationTestPostgreSQL.scala
+++
@@ -1,5 +0,0 @@
-package e2e.validation
-
-import e2e.PostgresEnabledTest
-
-class ValidationTestPostgreSQL extends PostgresEnabledTest with ValidationTest {}
--- src/test/scala/e2e/validation/ValidationTestSqlServer.scala
+++
@@ -1,5 +0,0 @@
-package e2e.validation
-
-import e2e.SqlServerEnabledTest
-
-class ValidationTestSqlServer extends SqlServerEnabledTest with ValidationTest {}
