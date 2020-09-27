3e22f7cd6da674a2c6238870839838d2dca13013
Teddy Widom <theodore.widom@gmail.com>
2020-09-27 11:41:14 -0400

    yah

--- src/main/scala/trw/dbsubsetter/Application.scala
+++ src/main/scala/trw/dbsubsetter/Application.scala
@@ -3,7 +3,6 @@ package trw.dbsubsetter
 object Application extends App {
   ApplicationRunner.run(args) match {
     case Success =>
-      System.out.println("Subsetting complete.")
       System.exit(0)
     case Error(message) =>
       System.err.println("Run failed with message: " + message)
