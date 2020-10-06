28da9e0308cc329c8de3449cb04c51921c8d64e0
Teddy Widom <theodore.widom@gmail.com>
2020-10-05 20:36:56 -0400

    Missing keys

--- src/test/scala/e2e/missingfk/MissingKeysTestSqlServer.scala
+++ src/test/scala/e2e/missingfk/MissingKeysTestSqlServer.scala
@@ -3,15 +3,12 @@ package e2e.missingfk
 import e2e.SqlServerSubsettingTest
 
 class MissingKeysTestSqlServer extends SqlServerSubsettingTest with MissingKeysTest {
-
+  // format: off
+  
   override val programArgs = Array(
-    "--schemas",
-    "dbo",
-    "--baseQuery",
-    "dbo.table_1 ::: id = 2 ::: includeChildren",
-    "--foreignKey",
-    "dbo.table_2(table_1_id) ::: dbo.table_1(id)",
-    "--primaryKey",
-    "dbo.table_4(table_1_id, table_3_id)"
+    "--schemas", "dbo",
+    "--baseQuery", "dbo.table_1 ::: id = 2 ::: includeChildren",
+    "--foreignKey", "dbo.table_2(table_1_id) ::: dbo.table_1(id)",
+    "--primaryKey", "dbo.table_4(table_1_id, table_3_id)"
   )
 }
