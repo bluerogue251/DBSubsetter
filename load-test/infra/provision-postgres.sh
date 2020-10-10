a94d3954360531569ea8b340f9c59fa16704c060
Teddy Widom <theodore.widom@gmail.com>
2020-10-10 17:48:35 -0400

    yah

--- load-test/infra/provision-postgres.sh
+++ load-test/infra/provision-postgres.sh
@@ -3,8 +3,13 @@
 set -eou pipefail
 
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt update
 apt install -y postgresql-10
-pg_ctlcluster 10 main start
-sudo -u postgres createuser ubuntu --superuser
+cat << EOF > /home/ubuntu/pg_hba.conf
+local    all    all                      peer
+host     all    all         0.0.0.0/0    md5
+host     all    all         ::/0         md5
+EOF
+pg_ctlcluster 10 main start -o "-c listen_addresses=* -c hba_file=/home/ubuntu/pg_hba.conf"
+sudo -u postgres createuser load-test --superuser --password load-test-pw
