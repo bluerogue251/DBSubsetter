f36429edf1ef978f30a4b61428432fc9992e4e27
Teddy Widom <theodore.widom@gmail.com>
2020-10-11 10:48:10 -0400

    yah

--- load-test/infra/provision-target.sh
+++ load-test/infra/provision-target.sh
@@ -23,12 +23,19 @@ sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt update
 apt install -y postgresql-10
+pg_ctlcluster 10 main stop
+pg_dropcluster 10 main
+
+#
+# Create load-test cluster
+#
 cat << EOF > /load-test/pg_hba.conf
 local    all    all                      peer
 host     all    all         0.0.0.0/0    md5
 host     all    all         ::/0         md5
 EOF
-pg_ctlcluster 10 main restart -o "-c listen_addresses=* -c hba_file=/load-test/pg_hba.conf"
+pg_createcluster 10 loadtest --datadir=/load-test/pg-data
+pg_ctlcluster 10 loadtest start -o "-c listen_addresses=* -c hba_file=/load-test/pg_hba.conf"
 sudo -u postgres psql -c "create role loadtest login superuser encrypted password 'load-test-pw'"
 
 #
