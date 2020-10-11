25cae3996735258a28c9318002dc83d509ad36e8
Teddy Widom <theodore.widom@gmail.com>
2020-10-10 23:48:17 -0400

    yah

--- run-load-test.sh
+++ run-load-test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#
+# Dependencies:
+#
+# 1) Docker installed locally
+#    https://docs.docker.com/get-docker/
+#
+# 2) AWS account credentials stored at ~/.aws/credentials
+#    https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+#
+
 echo "Creating SSH Key (if not exists)"
 echo "n" | ssh-keygen -q -N "" -f load-test/infra/load-test.pem -C "load-test@example.com"
 printf "\n\n"
