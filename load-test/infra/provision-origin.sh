dd65d544e0c942d830c96c32656b0a9df84395bb
Teddy Widom <theodore.widom@gmail.com>
2020-10-10 23:15:19 -0400

    yah

--- load-test/infra/provision-origin.sh
+++ load-test/infra/provision-origin.sh
@@ -14,6 +14,7 @@ set -eou pipefail
 mkfs -t xfs /dev/nvme1n1
 mkdir /load-test
 mount /dev/nvme1n1 /load-test
+chmod 777 /load-test
 
 #
 # Install Postgres
@@ -22,10 +23,10 @@ sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt update
 apt install -y postgresql-10
-cat << EOF > /home/ubuntu/pg_hba.conf
+cat << EOF > /load-test/pg_hba.conf
 local    all    all                      peer
 host     all    all         0.0.0.0/0    md5
 host     all    all         ::/0         md5
 EOF
-pg_ctlcluster 10 main restart -o "-c listen_addresses=* -c hba_file=/home/ubuntu/pg_hba.conf"
+pg_ctlcluster 10 main restart -o "-c listen_addresses=* -c hba_file=/load-test/pg_hba.conf"
 sudo -u postgres psql -c "create role loadtest login superuser encrypted password 'load-test-pw'"
--- load-test/infra/provision-target.sh
+++ load-test/infra/provision-target.sh
@@ -14,6 +14,7 @@ set -eou pipefail
 mkfs -t xfs /dev/nvme1n1
 mkdir /load-test
 mount /dev/nvme1n1 /load-test
+chmod 777 /load-test
 
 #
 # Install Postgres
@@ -22,30 +23,30 @@ sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt update
 apt install -y postgresql-10
-cat << EOF > /home/ubuntu/pg_hba.conf
+cat << EOF > /load-test/pg_hba.conf
 local    all    all                      peer
 host     all    all         0.0.0.0/0    md5
 host     all    all         ::/0         md5
 EOF
-pg_ctlcluster 10 main restart -o "-c listen_addresses=* -c hba_file=/home/ubuntu/pg_hba.conf"
+pg_ctlcluster 10 main restart -o "-c listen_addresses=* -c hba_file=/load-test/pg_hba.conf"
 sudo -u postgres psql -c "create role loadtest login superuser encrypted password 'load-test-pw'"
 
 #
 # Install Java
 #
-wget --quiet -O /home/ubuntu/jdk8.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz
-tar xzf /home/ubuntu/jdk8.tar.gz --directory=/home/ubuntu
-mv /home/ubuntu/jdk8u265-b01 /home/ubuntu/jdk8
+wget --quiet -O /load-test/jdk8.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz
+tar xzf /load-test/jdk8.tar.gz --directory=/load-test
+mv /load-test/jdk8u265-b01 /load-test/jdk8
 
 #
 # Install SBT
 #
-wget --quiet -O /home/ubuntu/sbt.tgz https://github.com/sbt/sbt/releases/download/v1.3.4/sbt-1.3.4.tgz
-tar xzf /home/ubuntu/sbt.tgz --directory=/home/ubuntu
+wget --quiet -O /load-test/sbt.tgz https://github.com/sbt/sbt/releases/download/v1.3.4/sbt-1.3.4.tgz
+tar xzf /load-test/sbt.tgz --directory=/load-test
 
 #
 # Build DBSubsetter
 #
-wget --quiet -O /home/ubuntu/DBSubsetter.tar.gz https://github.com/bluerogue251/DBSubsetter/archive/aba1435.tar.gz
-tar xzf /home/ubuntu/DBSubsetter.tar.gz --directory=/home/ubuntu
-cd /home/ubuntu/DBSubsetter-*; ./sbt/bin/sbt --java-home /home/ubuntu/jdk8 assembly; cd -
+wget --quiet -O /load-test/DBSubsetter.tar.gz https://github.com/bluerogue251/DBSubsetter/archive/aba1435.tar.gz
+tar xzf /load-test/DBSubsetter.tar.gz --directory=/load-test
+cd /load-test/DBSubsetter-*; ./sbt/bin/sbt --java-home /load-test/jdk8 assembly; cd -
