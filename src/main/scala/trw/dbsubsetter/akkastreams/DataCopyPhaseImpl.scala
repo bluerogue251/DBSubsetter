82846ed9c11fd60b4d6840736153dec2227f974a
Teddy Widom <theodore.widom@gmail.com>
2020-10-18 14:35:13 -0400

    Yah

--- src/main/scala/trw/dbsubsetter/akkastreams/DataCopyPhaseImpl.scala
+++ src/main/scala/trw/dbsubsetter/akkastreams/DataCopyPhaseImpl.scala
@@ -8,25 +8,37 @@ import trw.dbsubsetter.workflow.DataCopyTask
 
 final class DataCopyPhaseImpl(queue: DataCopyQueue, copiers: Seq[DataCopier]) extends DataCopyPhase {
 
+  private[this] val guard: Object = new Object()
+
   override def runPhase(): Unit = {
-    val executorService: ExecutorService = Executors.newFixedThreadPool(copiers.size)
-    val latch: CountDownLatch = new CountDownLatch(copiers.size)
+    val executorService: ExecutorService =
+      Executors.newFixedThreadPool(copiers.size)
+
+    val latch: CountDownLatch =
+      new CountDownLatch(copiers.size)
 
     copiers.foreach { copier =>
-      var nextTask = dequeueTask()
-      while (nextTask.nonEmpty) {
-        copier.runTask(nextTask.get)
-        nextTask = dequeueTask()
+      executorService.submit { () =>
+        copyTillExhausted(copier)
+        latch.countDown()
+        Unit
       }
-      latch.countDown()
     }
 
     latch.await()
     executorService.shutdownNow()
   }
 
+  private def copyTillExhausted(copier: DataCopier): Unit = {
+    var nextTask = dequeueTask()
+    while (nextTask.nonEmpty) {
+      copier.runTask(nextTask.get)
+      nextTask = dequeueTask()
+    }
+  }
+
   private def dequeueTask(): Option[DataCopyTask] = {
-    queue.synchronized {
+    guard.synchronized {
       queue.dequeue()
     }
   }
