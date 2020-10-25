dfdb614467362537231271aa5d978a06e68323ae
Teddy Widom <theodore.widom@gmail.com>
2020-10-25 11:09:09 -0400

    Try some stuff

--- src/main/scala/trw/dbsubsetter/pkstore/PrimaryKeyStoreInMemoryImpl.scala
+++ src/main/scala/trw/dbsubsetter/pkstore/PrimaryKeyStoreInMemoryImpl.scala
@@ -1,5 +1,8 @@
 package trw.dbsubsetter.pkstore
 
+import java.nio.ByteBuffer
+import java.util.UUID
+
 import trw.dbsubsetter.db.{PrimaryKeyValue, Table}
 
 import scala.collection.mutable
@@ -14,15 +17,15 @@ private[pkstore] final class PrimaryKeyStoreInMemoryImpl(tables: Seq[Table]) ext
    * is in there at all, then at any given time, it is either in `seenWithoutChildrenStorage` or in
    * `seenWithChildrenStorage` -- it will never be in both at once.
    */
-  private[this] val seenWithoutChildrenStorage: Map[Table, mutable.HashSet[Any]] =
+  private[this] val seenWithoutChildrenStorage: Map[Table, mutable.HashSet[ByteBuffer]] =
     PrimaryKeyStoreInMemoryImpl.buildStorage(tables)
 
-  private[this] val seenWithChildrenStorage: Map[Table, mutable.HashSet[Any]] =
+  private[this] val seenWithChildrenStorage: Map[Table, mutable.HashSet[ByteBuffer]] =
     PrimaryKeyStoreInMemoryImpl.buildStorage(tables)
 
   override def markSeen(table: Table, primaryKeyValue: PrimaryKeyValue): WriteOutcome = {
     this.synchronized {
-      val rawValue: Any = PrimaryKeyStoreInMemoryImpl.extract(primaryKeyValue)
+      val rawValue: ByteBuffer = PrimaryKeyStoreInMemoryImpl.extract(primaryKeyValue)
 
       val alreadySeenWithChildren: Boolean =
         seenWithChildrenStorage(table).contains(rawValue)
@@ -43,7 +46,7 @@ private[pkstore] final class PrimaryKeyStoreInMemoryImpl(tables: Seq[Table]) ext
 
   override def markSeenWithChildren(table: Table, primaryKeyValue: PrimaryKeyValue): WriteOutcome = {
     this.synchronized {
-      val rawValue: Any = PrimaryKeyStoreInMemoryImpl.extract(primaryKeyValue)
+      val rawValue: ByteBuffer = PrimaryKeyStoreInMemoryImpl.extract(primaryKeyValue)
 
       val alreadySeenWithChildren: Boolean =
         !seenWithChildrenStorage(table).add(rawValue)
@@ -64,22 +67,52 @@ private[pkstore] final class PrimaryKeyStoreInMemoryImpl(tables: Seq[Table]) ext
 
   override def alreadySeen(table: Table, primaryKeyValue: PrimaryKeyValue): Boolean = {
     this.synchronized {
-      val rawValue: Any = PrimaryKeyStoreInMemoryImpl.extract(primaryKeyValue)
+      val rawValue: ByteBuffer = PrimaryKeyStoreInMemoryImpl.extract(primaryKeyValue)
       seenWithChildrenStorage(table).contains(rawValue) || seenWithoutChildrenStorage(table).contains(rawValue)
     }
   }
 }
 
 private object PrimaryKeyStoreInMemoryImpl {
-  private def buildStorage(tables: Seq[Table]): Map[Table, mutable.HashSet[Any]] = {
-    tables.map { t => t -> mutable.HashSet.empty[Any] }.toMap
+  private def buildStorage(tables: Seq[Table]): Map[Table, mutable.HashSet[ByteBuffer]] = {
+    tables.map { t => t -> mutable.HashSet.empty[ByteBuffer] }.toMap
   }
 
-  private def extract(primaryKeyValue: PrimaryKeyValue): Any = {
+  private def extract(primaryKeyValue: PrimaryKeyValue): ByteBuffer = {
     if (primaryKeyValue.individualColumnValues.size == 1) {
-      primaryKeyValue.individualColumnValues.head
+      extractSingle(primaryKeyValue.individualColumnValues.head)
     } else {
-      primaryKeyValue.individualColumnValues
+      val buffers: Seq[ByteBuffer] = primaryKeyValue.individualColumnValues.map(extractSingle)
+      val count: Int = buffers.size
+      val bufferSizes: Seq[Int] = buffers.map(_.position())
+      /*
+       * 4 Bytes for the count
+       * 4 Bytes per buffer for its size
+       * Rest Bytes for all the contents
+       */
+      val capacity: Int = 4 + (4 * count) + bufferSizes.sum
+      val compositeBuffer: ByteBuffer = ByteBuffer.allocate(capacity)
+      compositeBuffer.putInt(count)
+      bufferSizes.foreach(compositeBuffer.putInt)
+      buffers.foreach(compositeBuffer.put)
+      compositeBuffer
+    }
+  }
+
+  private def extractSingle(value: Any): ByteBuffer = {
+    value match {
+      case _: Short  => ByteBuffer.allocate(2).putShort(value.asInstanceOf[Short])
+      case _: Int    => ByteBuffer.allocate(4).putInt(value.asInstanceOf[Int])
+      case _: Long   => ByteBuffer.allocate(8).putLong(value.asInstanceOf[Long])
+      case _: BigInt => ByteBuffer.wrap(value.asInstanceOf[BigInt].toByteArray)
+      case _: String => ByteBuffer.wrap(value.asInstanceOf[String].getBytes)
+      case _: UUID =>
+        val buffer: ByteBuffer = ByteBuffer.allocate(16)
+        val uuid: UUID = value.asInstanceOf[UUID]
+        buffer.putLong(uuid.getMostSignificantBits)
+        buffer.putLong(uuid.getLeastSignificantBits)
+        buffer
+      case _ => ByteBuffer.wrap(value.asInstanceOf[Array[Byte]])
     }
   }
 }
